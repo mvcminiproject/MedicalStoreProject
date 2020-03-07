58148c8050447fbb4384f4bdaeb92b9f53cd52e1
INDRANEEL <INDRANEEL@LAPTOP-F8IVI1JD>
2020-03-07 08:58:50 +0530

    Added toString in cfgs Pkg

--- MiniProjectStore/src/main/java/com/nt/cfgs/MyFirstClass.java
+++ MiniProjectStore/src/main/java/com/nt/cfgs/MyFirstClass.java
@@ -4,4 +4,10 @@ public class MyFirstClass {
 	public static void main(String[] args) {
 		System.out.println("First Branch");
 	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
 }
